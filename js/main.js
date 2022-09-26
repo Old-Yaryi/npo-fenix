3d75a8375e1614743f81f41563389d504e06a30a
Old-Yaryi <kinomir-v@yandex.ru>
2022-09-26 18:09:29 +0300

    'mobile_menu'

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
+// *****Языковое меню******
 const lang = document.querySelector('.header__menu-link_language');
 const lang_ul = document.querySelector('.header__menu-link_language-var');
 lang.addEventListener('click', function(){
   lang_ul.classList.toggle('active')
 });
+const lang_mobile = document.querySelector('.mobile__info-social_language');
+const lang_ul_mobile = document.querySelector('.mobile__info-social_language-var');
+lang_mobile.addEventListener('click', function(){
+  lang_ul_mobile.classList.toggle('active')
+});
+// *******Мобильное меню*******
+const mobile_menu = document.querySelectorAll('.burger');
+const mobile_menu_open = document.querySelector('.mobile');
+const mobile_menu_close = document.querySelector('.mobile__close');
+mobile_menu.forEach(el => {
+    el.addEventListener('click', function(){
+    mobile_menu_open.classList.toggle('mobile-active')
+  });
+});
+mobile_menu_close.addEventListener('click', function() {
+  mobile_menu_open.classList.toggle('mobile-active')
+});
+

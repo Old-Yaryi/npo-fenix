10ec6760cf7a3e2fc80402707e52f7f256d15532
Old-Yaryi <kinomir-v@yandex.ru>
2022-11-10 19:09:43 +0300

    'snab_480_begin'

--- js/snab.js
+++ js/snab.js
@@ -3,3 +3,9 @@ $('.supply-license-img').fancybox({
   toolbar: "smallBtn",
 	arrows: true,
 });
+$(".procurement__price .procurement__price-header").on("click", function() {
+  if ($(window).width() <= 680){
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();   
+  }
+});

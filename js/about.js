8ece075e4337e0ed262f321682550df44b6a90a5
Old-Yaryi <kinomir-v@yandex.ru>
2022-11-17 17:23:35 +0300

    'about_desctop'

--- js/about.js
+++ js/about.js
@@ -41,25 +41,25 @@ $('.about-sertificate__slider').slick({
     }
   ]
 });
-// const swiper = new Swiper('.swiper', {
-//   slidesPerView: 5,
-//   // centeredSlides: true,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   breakpoints: {
-//     // when window width is >= 320px
-//     320: {
-//       slidesPerView: 3,
 
-//     },
-//     415: {
-//       slidesPerView: 3,
-//     },
-//     480: {
-//       slidesPerView: 5,
-//     },
-//   }
-// });
+$('.about-reviews__slider').slick({
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  appendArrows:'.about-reviews__nav',
+  speed: 400,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
